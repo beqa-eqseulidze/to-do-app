f885a6e46a103bcb2fa8155bc3b7e7af77278f13
beqa-eqseulidze <beqaeqseulidze6@gmail.com>
2024-10-22 20:24:13 +0400

    creatre to-do app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateEditComponent } from './create-edit/create-edit.component';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { CustomDatePipe } from './pipes/custom-date.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEditComponent,
+    ToDoListComponent,
+    CustomDatePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterOutlet
+
   ],
   providers: [],
   bootstrap: [AppComponent]
